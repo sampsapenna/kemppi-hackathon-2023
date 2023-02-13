2a4b8fabc80a55bf52dbcd07ba4e20921d4c818b
Jonna Konttila <jonna.konttila@gmail.com>
2023-02-13 14:37:26 +0200

    initial main app

--- hackathon-frontend/src/apps/App.vue
+++ hackathon-frontend/src/apps/App.vue
@@ -1,19 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <md-table v-model="searched" md-sort="name" md-sort-order="asc" md-card md-fixed-header>
+      <md-table-toolbar>
+        <div class="md-toolbar-section-start">
+          <h1 class="md-title">Sovellukset</h1>
+        </div>
+
+        <md-field md-clearable class="md-toolbar-section-end">
+          <md-input placeholder="Search by name..." v-model="search" @input="searchOnTable" />
+        </md-field>
+      </md-table-toolbar>
+
+      <md-table-empty-state md-label="No users found"
+        :md-description="`No user found for this '${search}' query. Try a different search term or create a new user.`">
+        <md-button class="md-primary md-raised" @click="newUser">Create New User</md-button>
+      </md-table-empty-state>
+
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
+        <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
+      </md-table-row>
+    </md-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
 
 export default {
-  name: 'MainApp',
-  components: {
-    HelloWorld
+  name: 'TableSearch',
+  data: () => ({
+    search: null,
+    searched: [],
+    users: [
+      {
+        id: 1,
+        name: "Sovellus 1",
+      },
+      {
+        id: 2,
+        name: "Sovellus 2",
+      }
+    ]
+  }),
+  methods: {
+    newUser() {
+      window.alert('Noop')
+    },
+    searchOnTable() {
+      this.searched = this.searchByName(this.users, this.search)
+    },
+
+    toLower(text){
+    return text.toString().toLowerCase()
+    },
+
+    searchByName(items, term){
+    if (term) {
+    return items.filter(item => this.toLower(item.name).includes(this.toLower(term)))
+    }
+
+  return items
+}
+  },
+  created() {
+    this.searched = this.users
   }
 }
+
 </script>
 
 <style>
--- hackathon-frontend/src/entries/app.js
+++ hackathon-frontend/src/entries/app.js
@@ -4,11 +4,14 @@ import { VueMaterial } from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
-import MainApp from '@/apps/App.vue'
+
+import TableSearch from '@/apps/App.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
 
 new Vue({
-  render: h => h(MainApp),
+  render: h => h(TableSearch),
+  
+
 }).$mount('#app')
