f4c6185bce4dbff5cd51eb818f19e6b13febcf50
Heidi Paananen <heidi.paananen@student.lab.fi>
2023-02-13 21:43:27 +0200

    Login

--- hackathon-frontend/src/entries/login.js
+++ hackathon-frontend/src/entries/login.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 
-import IndexApp from '@/apps/Index'
+import LoginApp from '@/apps/Login'
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import {
+  BootstrapVue,
+  IconsPlugin
+} from 'bootstrap-vue'
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-import HelloWorld from '@/components/HelloWorld.vue'
 
 Vue.config.productionTip = false
 // Make BootstrapVue available throughout your project
@@ -17,9 +18,35 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 new Vue({
-  name: "index-page",
-  components: {
-    HelloWorld,
+  name: "login-page",
+  components: {},
+  ...LoginApp,
+
+  data: {
+    registerActive: false,
+    emailLogin: "",
+    passwordLogin: "",
+    emailReg: "",
+    passwordReg: "",
+    confirmReg: "",
+    emptyFields: false
   },
-  ...IndexApp,
+
+  methods: {
+    doLogin() {
+      if (this.emailLogin === "" || this.passwordLogin === "") {
+        this.emptyFields = true;
+      } else {
+        alert("You are now logged in");
+      }
+    },
+
+    doRegister() {
+      if (this.emailReg === "" || this.passwordReg === "" || this.confirmReg === "") {
+        this.emptyFields = true;
+      } else {
+        alert("You are now registered");
+      }
+    }
+  }
 }).$mount('#app')
