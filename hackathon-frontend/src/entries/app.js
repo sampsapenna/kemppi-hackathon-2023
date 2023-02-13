00cd2529c72bc1f8ea1683b9329580a4eb136652
Sampsa Penna <sampsapenna@gmail.com>
2023-02-13 11:30:13 +0200

    import vue material design components

--- hackathon-frontend/src/entries/app.js
+++ hackathon-frontend/src/entries/app.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+
+import { VueMaterial } from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
 import App from '@/Apps/App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
 
 new Vue({
   render: h => h(App),
